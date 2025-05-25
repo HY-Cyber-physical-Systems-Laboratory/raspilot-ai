d723b6f23645b5cabe465cf1ef6905fee7e20224
chw1119 <chw1119@hanyang.ac.kr>
2025-05-26 04:43:05 +0900

    [new] add new anotation

--- src/test.c
+++ src/test.c
@@ -10,28 +10,25 @@
 
 int main()
 {
-    const char* PYTHON_EMBEDDING_CODE = R"(
-                import sys
-                def any_drone_utility_func(args):
-                    print('Hello from embedded Python!')
-                    print('Arguments:', args)
 
-                    return args
-            )";
+    //todo : auto python embedding code generation
+    const char* PYTHON_EMBEDDING_CODE = 
+    #include "./python/video/face_distortion_detect.py" // add your Python code's head and tail R"()"
+    ;
             
-            Py_Initialize();
+    Py_Initialize();
 
-            PyObject *code = PY_COMPILE("def hello(name): print('Hello,', name)");
-            PyObject *mod = PY_EXEC(code);
-            PyObject *func = PY_GET_FUNC(mod, "hello");
+    PyObject *code = PY_COMPILE("def hello(name): print('Hello,', name)");
+    PyObject *mod = PY_EXEC(code);
+    PyObject *func = PY_GET_FUNC(mod, "hello");
 
-            PyObject *args = PyTuple_Pack(1, PyUnicode_FromString("Pilot"));
-            PY_CALL(func, args);
+    PyObject *args = PyTuple_Pack(1, PyUnicode_FromString("Pilot"));
+    PY_CALL(func, args);
 
-            Py_XDECREF(args);
-            Py_XDECREF(func);
-            Py_XDECREF(mod);
-            Py_XDECREF(code);
+    Py_XDECREF(args);
+    Py_XDECREF(func);
+    Py_XDECREF(mod);
+    Py_XDECREF(code);
 
-            Py_Finalize();
+    Py_Finalize();
 }
