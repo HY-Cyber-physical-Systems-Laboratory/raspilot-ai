463f187f05927e43925314d208cdc18fa95e2460
chw1119 <chw1119@hanyang.ac.kr>
2025-05-31 04:41:13 +0900

    sibal

--- tool/imu/gy-85/adxl345+hcm5883l+itg3200.c
+++ tool/imu/gy-85/adxl345+hcm5883l+itg3200.c
@@ -437,7 +437,7 @@ static int gConfigure(int gFile)
 }
 
 int main(int argc, char **argv) {
-    setbuf(stdout, NULL); 
+    //setbuf(stdout, NULL); 
     double 	t0, t1, samplePeriod;
     int		i, usleepTime;
     int		magFd;
